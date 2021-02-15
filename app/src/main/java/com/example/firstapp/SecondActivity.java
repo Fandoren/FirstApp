e0f467b7d17e789150f1fe785b4c339ff16bb4b7
AleshaMolodec <aleks.fandoren@gmail.com>
2021-02-16 01:21:05 +0300

    Задание из лекции и задание по двум видео

---
+++ app/src/main/java/com/example/firstapp/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setText(message);
+
+        setContentView(textView);
+    }
+}
